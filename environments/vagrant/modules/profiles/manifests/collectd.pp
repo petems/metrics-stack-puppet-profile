3ea8ba46048d46f741481595e1f4bdff8c5dd832
Peter Souter <p.morsou@gmail.com>
2016-02-24 19:15:23 +0000

    Changes collectd plugins to use puppet types

--- environments/vagrant/modules/profiles/manifests/collectd.pp
+++ environments/vagrant/modules/profiles/manifests/collectd.pp
@@ -11,4 +11,21 @@ class profiles::collectd {
     conf_content   => template('profiles/collectd.conf.erb'),
   }
 
+  class { '::collectd::plugin::logfile':
+    log_level => 'warning',
+    log_file  => '/var/log/collected.log'
+  }
+
+  class { '::collectd::plugin::interface':
+    interfaces     => ['eth0'],
+    ignoreselected => false,
+  }
+
+  class { '::collectd::plugin::load':
+  }
+
+  collectd::plugin::network::server{'127.0.0.1':
+    port => '25826',
+  }
+
 }
